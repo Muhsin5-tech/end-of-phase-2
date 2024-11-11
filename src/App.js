2d51435c4fea4a3c1e65458dcfa66f31a0694e0e
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-11-11 21:03:09 +0300

    Adds db.json src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  const [destinations, setDestinations] = useState([])
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:3001/destinations")
+    .then((response) => response.json())
+    .then((data) => setDestinations(data))
+    .catch((error) => console.error('Error fetching data', error))
+  })
+  
+  
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Travel Bucket List App</h1>
+     <ul>
+     {destinations.map((destination) => (
+          <li key={destination.id}>
+          <h2>{destination.name}</h2>
+          <p>Visited: {destination.visited ? "Yes" : "No"}</p>
+          <p>Notes: {destination.notes}</p>
+        </li>
+        ))}
+     </ul>
     </div>
   );
 }
